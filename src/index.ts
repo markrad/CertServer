51b5d85350502d89d910574752bf6b9be6238220
Mark Radbourne <markrad@msn.com>
2023-07-05 16:01:52 -0700

    :fire: Remove todo comment

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import { WebServer } from './webserver';
 
 // const ROOT_DIRECTORY = '/workspaces/typescript-node/data';
-// TODO: Add config file or parameters
 // const ROOT_DIRECTORY = path.join(__dirname, '../data');
 // const SERVER_PORT = 5501;
 
