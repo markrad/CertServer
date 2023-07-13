b348efd16364a7d185da561a1f73ad36a6c617ac
Mark Radbourne <markrad@msn.com>
2023-07-13 15:05:37 -0700

    :bug: Bug fix

--- src/utility/exists.ts
+++ src/utility/exists.ts
@@ -1,13 +1,13 @@
 import { access, constants } from 'fs/promises';
 
 export async function exists(filename: string): Promise<boolean> {
-    return new Promise<boolean>((resolve, reject) => {
+    return new Promise<boolean>(async (resolve, _reject) => {
         try {
-            access(filename, constants.F_OK);
+            await access(filename, constants.F_OK);
             resolve(true);
         }
         catch (err) {
-            reject(false);
+            resolve(false);
         }
     });
 }
