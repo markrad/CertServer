04bfbaa37f10daaa8882b22c46fc973fd556e161
Mark Radbourne <markrad@msn.com>
2023-12-06 12:16:27 -0800

    :fire: Remove unused file

--- web/files/helpers.sh
+++
@@ -1,7 +0,0 @@
-echo function getcert\(\){ wget --content-disposition https://rr-frigate.lan:4141/api/getcertificatepem?id=\$@\; } >> ~/.bash_aliases
-echo function getkey\(\){ wget --content-disposition https://rr-frigate.lan:4141/api/getkeypem?id=\$@\; } >> ~/.bash_aliases
-echo function getchain\(\){ wget --content-disposition https://rr-frigate.lan:4141/api/chaindownload?id=\$@\; } >> ~/.bash_aliases
-echo Shortcuts added - run:
-echo source ~/.bash_aliases 
-echo to load
-echo Most Debian based distros will source .bash_aliases .bash_rc so you only need to do this once.
