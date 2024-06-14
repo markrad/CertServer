5fea97530a5f0e7a9011e57e522df57cff37dd02
Mark Radbourne <markrad@msn.com>
2024-06-14 23:18:16 +0000

    :whale: Update devcontainer to include an additional root cert

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,6 +2,7 @@
 echo "Installing dependencies..."
 sudo cp ./.devcontainer/radrealm.crt /usr/local/share/ca-certificates/radrealm.crt
 sudo update-ca-certificates
-tsc --watch
+# tsc --watch &
+# bash -c node /usr/local/share/npm-global/lib/node_modules/typescript/lib/tsc.js -w &
 
 
