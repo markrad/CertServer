5b8c13527c935cf5d2c489eccac3c1dd5b7a748e
Mark Radbourne <markrad@msn.com>
2024-06-20 18:33:57 +0000

    :sparkles: Add user manipulation functions

--- output/src/database/userStore.js
+++ output/src/database/userStore.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserStore = void 0;
 const CertError_1 = require("../webservertypes/CertError");
+const UserRole_1 = require("./UserRole");
 const bcrypt_1 = __importDefault(require("bcrypt"));
 class UserStore {
     static init(certificateDb) {
@@ -13,6 +14,7 @@ class UserStore {
         UserStore._userDb = certificateDb;
     }
     static authenticate(username, password) {
+        var _a;
         if (UserStore._userDb == null)
             throw new CertError_1.CertError(500, "UserStore not initialized");
         if (!username)
@@ -20,14 +22,62 @@ class UserStore {
         if (!password)
             throw new CertError_1.CertError(400, "Password is required");
         let user = UserStore._userDb.findOne({ username: username });
-        return user != null && bcrypt_1.default.compareSync(password, user.password);
+        if (user != null || !bcrypt_1.default.compareSync(password, user.password))
+            throw new CertError_1.CertError(401, "Invalid username or password");
+        return (_a = user.role) !== null && _a !== void 0 ? _a : UserRole_1.UserRole.USER;
     }
     static getUser(username) {
         if (UserStore._userDb == null)
             throw new CertError_1.CertError(500, "UserStore not initialized");
         if (!username)
             throw new CertError_1.CertError(400, "Username is required");
-        return UserStore._userDb.findOne({ username: username }) != null;
+        return UserStore._userDb.findOne({ username: username });
+    }
+    static getAllUsers() {
+        if (UserStore._userDb == null)
+            throw new CertError_1.CertError(500, "UserStore not initialized");
+        return UserStore._userDb.find();
+    }
+    static addUser(username, password, role) {
+        if (UserStore._userDb == null)
+            throw new CertError_1.CertError(500, "UserStore not initialized");
+        if (!username)
+            throw new CertError_1.CertError(400, "Username is required");
+        if (!password)
+            throw new CertError_1.CertError(400, "Password is required");
+        let user = UserStore._userDb.findOne({ username: username });
+        if (user)
+            throw new CertError_1.CertError(400, `User ${username} already exists`);
+        UserStore._userDb.insert({
+            username: username,
+            password: bcrypt_1.default.hashSync(password, 10),
+            role: role,
+            lastSignedIn: null,
+            tokenExpiration: null
+        });
+    }
+    static removeUser(username) {
+        if (UserStore._userDb == null)
+            throw new CertError_1.CertError(500, "UserStore not initialized");
+        if (!username)
+            throw new CertError_1.CertError(400, "Username is required");
+        let user = UserStore._userDb.findOne({ username: username });
+        if (!user)
+            throw new CertError_1.CertError(400, `User ${username} not found`);
+        UserStore._userDb.remove(user);
+    }
+    static updatePassword(username, password) {
+        if (UserStore._userDb == null)
+            throw new CertError_1.CertError(500, "UserStore not initialized");
+        if (!username)
+            throw new CertError_1.CertError(400, "Username is required");
+        if (!password)
+            throw new CertError_1.CertError(400, "Password is required");
+        let user = UserStore._userDb.findOne({ username: username });
+        if (!user)
+            throw new CertError_1.CertError(400, `User ${username} not found`);
+        user.password = bcrypt_1.default.hashSync(password, 10);
+        UserStore._userDb.update(user);
     }
 }
 exports.UserStore = UserStore;
--- src/database/userStore.ts
+++ src/database/userStore.ts
@@ -1,4 +1,5 @@
 import { CertError } from "../webservertypes/CertError";
+import { UserRole } from "./UserRole";
 import { UserRow } from "./UserRow";
 import bcrypt from 'bcrypt';
 
@@ -11,20 +12,71 @@ export class UserStore {
         UserStore._userDb = certificateDb;
     }
 
-    public static authenticate(username: string, password: string): boolean {
+    public static authenticate(username: string, password: string): UserRole {
         if (UserStore._userDb == null) throw new CertError(500, "UserStore not initialized");
         if (!username) throw new CertError(400, "Username is required");
         if (!password) throw new CertError(400, "Password is required");
 
         let user = UserStore._userDb.findOne({ username: username });
 
-        return user != null && bcrypt.compareSync(password, user.password);
+        if (user != null || !bcrypt.compareSync(password, user.password)) throw new CertError(401, "Invalid username or password");
+
+        return user.role ?? UserRole.USER;
     }
 
-    public static getUser(username: string): boolean {
+    public static getUser(username: string): UserRow {
         if (UserStore._userDb == null) throw new CertError(500, "UserStore not initialized");
         if (!username) throw new CertError(400, "Username is required");
 
-        return UserStore._userDb.findOne({ username: username }) != null;
+        return UserStore._userDb.findOne({ username: username });
+    }
+
+    public static getAllUsers(): UserRow[] {
+        if (UserStore._userDb == null) throw new CertError(500, "UserStore not initialized");
+
+        return UserStore._userDb.find();
+    }
+
+    public static addUser(username: string, password: string, role: UserRole): void {
+        if (UserStore._userDb == null) throw new CertError(500, "UserStore not initialized");
+        if (!username) throw new CertError(400, "Username is required");
+        if (!password) throw new CertError(400, "Password is required");
+
+        let user = UserStore._userDb.findOne({ username: username });
+
+        if (user) throw new CertError(400, `User ${username} already exists`);
+
+        UserStore._userDb.insert({
+            username: username,
+            password: bcrypt.hashSync(password, 10),
+            role: role,
+            lastSignedIn: null,
+            tokenExpiration: null
+        });
+    }
+
+    public static removeUser(username: string): void {
+        if (UserStore._userDb == null) throw new CertError(500, "UserStore not initialized");
+        if (!username) throw new CertError(400, "Username is required");
+
+        let user = UserStore._userDb.findOne({ username: username });
+
+        if (!user) throw new CertError(400, `User ${username} not found`);
+
+        UserStore._userDb.remove(user);
+    }
+
+    public static updatePassword(username: string, password: string): void {
+        if (UserStore._userDb == null) throw new CertError(500, "UserStore not initialized");
+        if (!username) throw new CertError(400, "Username is required");
+        if (!password) throw new CertError(400, "Password is required");
+
+        let user = UserStore._userDb.findOne({ username: username });
+
+        if (!user) throw new CertError(400, `User ${username} not found`);
+
+        user.password = bcrypt.hashSync(password, 10);
+
+        UserStore._userDb.update(user);
     }
 }
