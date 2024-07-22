b9054c80bee263b69f101e3d2257f3d2d1bb8868
Mark Radbourne <markrad@msn.com>
2024-07-22 20:23:47 +0000

    :whale: Add alias to dev container

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,6 +2,10 @@
 echo "Installing dependencies..."
 sudo cp ./.devcontainer/radrealm.crt /usr/local/share/ca-certificates/radrealm.crt
 sudo update-ca-certificates
+# Set up aliases
+touch /home/node/.bash_aliases
+echo alias ll=\'ls -alF\' >> /home/node/.bash_aliases
+#chmod +x /home/node/.bashrc
 # tsc --watch &
 # bash -c node /usr/local/share/npm-global/lib/node_modules/typescript/lib/tsc.js -w &
 
